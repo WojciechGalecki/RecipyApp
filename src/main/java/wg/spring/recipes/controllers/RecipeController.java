5feb44a6cb77ac794ddea698c8065c946cce4529
Wojtek <wojciech.galecki@wp.pl>
2018-05-06 16:49:00 +0200

    adding implementation for showById methon in RecipeController. Now test for this is OK

--- src/main/java/wg/spring/recipes/controllers/RecipeController.java
+++ src/main/java/wg/spring/recipes/controllers/RecipeController.java
@@ -1,6 +1,9 @@
 package wg.spring.recipes.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import wg.spring.recipes.services.RecipeService;
 
 @Controller
@@ -12,4 +15,11 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+
+        return "recipe/show";
+    }
 }
